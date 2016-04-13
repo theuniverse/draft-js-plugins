f0b4c4d02d7deb831536e03d9fc1685dc0712795
theuniverse <luckyqi2000@gmail.com>
2016-04-14 03:34:07 +0800

    enable inline style cascading

--- draft-js-markup-plugin/src/modifiers/applyStyle.js
+++ draft-js-markup-plugin/src/modifiers/applyStyle.js
@@ -1,10 +1,11 @@
-import { Modifier, EditorState } from 'draft-js';
+import { Modifier, EditorState, RichUtils } from 'draft-js';
 
 const applyStyle = (editorState, start, end, content, style) => {
   const currentSelectionState = editorState.getSelection();
+  const cursorPos = currentSelectionState.getAnchorOffset();
 
   // get selection of the @mention search text
-  const removeTextSelection = currentSelectionState.merge({
+  /*const removeTextSelection = currentSelectionState.merge({
     anchorOffset: start,
     focusOffset: end,
   });
@@ -17,8 +18,7 @@ const applyStyle = (editorState, start, end, content, style) => {
 
   const styledTextSelection = removeTextSelection.merge({
     anchorOffset: start,
-    focusOffset: end - 2,
-    hasFocus: false
+    focusOffset: end - 2
   });
 
   styledTextReplacedContent = Modifier.applyInlineStyle(
@@ -31,8 +31,52 @@ const applyStyle = (editorState, start, end, content, style) => {
     editorState,
     styledTextReplacedContent,
     'apply-style',
+  );*/
+
+  const styledTextSelection = currentSelectionState.merge({
+    anchorOffset: start + 1,
+    focusOffset: end - 1
+  });
+
+  const styledEditorState = RichUtils.toggleInlineStyle(
+    EditorState.acceptSelection(
+      editorState,
+      styledTextSelection
+    ),
+    style
   );
-  return EditorState.forceSelection(newEditorState, styledTextReplacedContent.getSelectionAfter());
+
+  const styledContentState = styledEditorState.getCurrentContent();
+  let finalContentState = Modifier.removeRange(
+    styledContentState,
+    currentSelectionState.merge({
+      anchorOffset: end - 1,
+      focusOffset: end
+    }),
+    'forward'
+  );
+  finalContentState = Modifier.removeRange(
+    finalContentState,
+    currentSelectionState.merge({
+      anchorOffset: start,
+      focusOffset: start + 1
+    }),
+    'forward'
+  );
+
+  const newEditorState = EditorState.push(
+    styledEditorState,
+    finalContentState,
+    'apply-style',
+  );
+
+  const newCursorPos = ((cursorPos - 1) === start ? start : end - 2);
+  const lastTextSelection = styledTextSelection.merge({
+    anchorOffset: newCursorPos,
+    focusOffset: newCursorPos
+  });
+
+  return EditorState.forceSelection(newEditorState, lastTextSelection);
 };
 
 export default applyStyle;
