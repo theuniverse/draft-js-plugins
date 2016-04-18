09d9e66d0e67b46dc6e326529b90482af04b2464
Qi Xi <xiqish@cn.ibm.com>
2016-04-18 17:43:25 +0800

    keep * and _ in code

--- draft-js-markup-plugin/src/createMarkupComponent.js
+++ draft-js-markup-plugin/src/createMarkupComponent.js
@@ -3,7 +3,7 @@ import { genKey } from 'draft-js';
 import decorateComponentWithProps from 'decorate-component-with-props';
 
 import applyStyle from './modifiers/applyStyle';
-import getBlockText from './utils/getBlockText';
+import getSelectedBlock from './utils/getSelectedBlock';
 
 export default (config = {}) => {
   class MarkupComponnt extends Component {
@@ -22,13 +22,25 @@ export default (config = {}) => {
     onEditorStateChange(syntax, style) {
       return function(editorState) {
         const selection = editorState.getSelection();
-        const word = getBlockText(editorState, selection);
+        const selectedBlock = getSelectedBlock(editorState, selection);
+        const blockText = selectedBlock.getText();
 
         let matchArr;
-        if ((matchArr = syntax.exec(word)) !== null) { // eslint-disable-line
+        if ((matchArr = syntax.exec(blockText)) !== null) { // eslint-disable-line
           let start = matchArr.index; // eslint-disable-line
-          let end = start + matchArr[0].length;
-          let content = word.substring(start+1, end-1);
+          let end = start + matchArr[0].length; // eslint-disable-line
+
+          let startStyle = selectedBlock.getInlineStyleAt(start);
+          if (startStyle.has('CODE')) {
+            return editorState;
+          }
+
+          let endStyle = selectedBlock.getInlineStyleAt(start);
+          if (endStyle.has('CODE')) {
+            return editorState;
+          }
+
+          let content = blockText.substring(start+1, end-1);
           let newEditorState = editorState;
           return applyStyle(newEditorState, start, end, content, style);
         }
