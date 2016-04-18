f2c4c4e90d7a72af47a50922d271e1e6a0070250
Qi Xi <xiqish@cn.ibm.com>
2016-04-18 17:44:08 +0800

    keep * and _ in code

--- draft-js-markup-plugin/src/utils/getBlockText.js
+++
@@ -1,8 +0,0 @@
-const getBlockText = (editorState, selection) => {
-  const anchorKey = selection.getAnchorKey();
-  const currentContent = editorState.getCurrentContent();
-  const currentBlock = currentContent.getBlockForKey(anchorKey);
-  return currentBlock.getText();
-};
-
-export default getBlockText;
